c4f581335bf4e81f8464bced58805292eac84082
StepSt <legionst@gmail.com>
2016-04-14 17:01:50 +0500

    Использовал шрифт

--- app/src/main/java/com/example/admin/pipes/Main.java
+++ app/src/main/java/com/example/admin/pipes/Main.java
@@ -1,7 +1,9 @@
 package com.example.admin.pipes;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "Roboto-Regular.ttf");
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setTypeface(myTypeface);
     }
 }
