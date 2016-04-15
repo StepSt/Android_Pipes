d9fddc41d8a528aa135c295a0147ec9d74091ff2
StepSt <legionst@gmail.com>
2016-04-15 13:04:46 +0500

    Функционал калькулятора

--- app/src/main/java/com/example/admin/pipes/Calc.java
+++ app/src/main/java/com/example/admin/pipes/Calc.java
@@ -5,7 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class Calc extends AppCompatActivity {
 
@@ -13,6 +19,15 @@ public class Calc extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+        final String[] pipes_type = new String[] {
+                "Круглые трубы", "Прямоугольные трубы"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,	android.R.layout.simple_list_item_1, pipes_type);
+
+        listView.setAdapter(adapter);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,5 +56,18 @@ public class Calc extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void Clic_Result (View view)
+    {
+        EditText edit_D = (EditText) findViewById(R.id.edit_D);
+        EditText edit_S = (EditText) findViewById(R.id.edit_S);
+        EditText edit_L = (EditText) findViewById(R.id.edit_L);
+        Double res_pM = ((Double.parseDouble(edit_D.getText().toString()) - Double.parseDouble(edit_S.getText().toString()))*Double.parseDouble(edit_S.getText().toString()))/40.65;
+        TextView text_pM = (TextView) findViewById(R.id.text_pM);
+        text_pM.setText(res_pM.toString());
+        Double res_M = res_pM*Double.parseDouble(edit_L.getText().toString());
+        TextView text_M = (TextView) findViewById(R.id.text_M);
+        text_M.setText(res_M.toString());
     }
 }
